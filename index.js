767de6c5e0c562643b6eef9a0eab0f804778ca0a
geearen <allengonzales018@gmail.com>
2023-03-13 20:41:14 -0700

    add eslint and new package, start on index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
+import { Client, Intents, Collections } from 'discord.js';
+import { Player } from 'discord-player';
+
+require('dotenv').config();
